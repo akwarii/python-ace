3113094a8ac71048e9af8bd1f96cb57d8ba62f6b
ghuynh <gael.huynh@gmail.com>
2024-11-02 22:21:52 +0100

    Add deprecation decorator

--- src/pyace/utils/utils.py
+++ src/pyace/utils/utils.py
@@ -1,13 +1,14 @@
+import functools
 import logging
+import warnings
 
-from ase.data import covalent_radii, atomic_numbers
+from ase.data import atomic_numbers, covalent_radii
+
 from pyace.atomicenvironment import aseatoms_to_atomicenvironment
 
 
 def compute_nn_dist_per_bond(atoms, cutoff=3, elements_mapper_dict=None):
-    ae = aseatoms_to_atomicenvironment(
-        atoms, cutoff, elements_mapper_dict=elements_mapper_dict
-    )
+    ae = aseatoms_to_atomicenvironment(atoms, cutoff, elements_mapper_dict=elements_mapper_dict)
     return ae.get_minimal_nn_distance_per_bond()
 
 
@@ -18,9 +19,7 @@ def get_vpa(atoms):
         return 0
 
 
-def complement_min_dist_dict(
-    min_dist_per_bond_dict, bond_quantile_dict, elements, verbose
-):
+def complement_min_dist_dict(min_dist_per_bond_dict, bond_quantile_dict, elements, verbose):
     res = min_dist_per_bond_dict.copy()
     for mu_i in range(len(elements)):
         for mu_j in range(mu_i, len(elements)):
@@ -40,3 +39,22 @@ def complement_min_dist_dict(
                         )
                     res[k] = r_in
     return res
+
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter("always", DeprecationWarning)  # turn off filter
+        warnings.warn(
+            f"Call to deprecated function {func.__name__}.",
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        warnings.simplefilter("default", DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+
+    return new_func
